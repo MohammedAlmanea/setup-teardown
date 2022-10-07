17a1709b601427bdf9384fa0e387cd96d1e37a27
MohammedAlmanea <xrkodx1@gmail.com>
2022-10-07 14:47:53 +0300

    Fixed bug in addArr test number 2

--- src/tests/utilities/arraysSpec.ts
+++ src/tests/utilities/arraysSpec.ts
@@ -8,8 +8,8 @@ describe('testing arrays.ts functions', () => {
     it('should add numbers in array and be truthy', () => {
       expect(arrays.addArr(numArr)).toBeTruthy();
     });
-    it('should add numbers in array and be 19', () => {
-      expect(arrays.addArr(numArr)).toBe(19);
+    it('should add numbers in array and be 18', () => {
+      expect(arrays.addArr(numArr)).toBe(18);
     });
   });
 
